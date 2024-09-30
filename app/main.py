81faac8e51c49509cb354a1b19226050b31a105d
bennyocean <benjamin.schellinger@web.de>
2024-09-30 07:24:10 -0500

    update route importes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import joblib
 import logging
-from app.routes import prediction, evaluation, database, token_generator, create_user, get_users, user
+from app.routes import prediction, evaluation, database, user
 
 app = FastAPI()
 
@@ -13,9 +13,6 @@ with open('model/model.pkl', 'rb') as f:
     model = joblib.load(f)
 
 # routes
-#app.include_router(token_generator.router)
-#app.include_router(create_user.router)
-#app.include_router(get_users.router)
 app.include_router(user.router)
 app.include_router(database.router)
 app.include_router(prediction.router)
