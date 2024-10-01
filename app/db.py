3ec37668488b1fad28f157d76d55083fde8b3419
bennyocean <benjamin.schellinger@web.de>
2024-09-30 21:04:41 -0500

    check db.py

--- app/db.py
+++ app/db.py
@@ -3,18 +3,10 @@ from dotenv import load_dotenv
 import os
 from bson import ObjectId
 
-# Helper function to convert MongoDB ObjectId to string
-'''
-def object_id_to_str(doc):
-    if '_id' in doc:
-        doc['_id'] = str(doc['_id'])
-    return doc
-
-load_dotenv()
-'''
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+
 client = MongoClient(MONGO_URI)
 
 db = client["lfb"]
