23211cf7223f3c35d6e8d8ee7aac784a96206171
bennyocean <benjamin.schellinger@web.de>
2024-09-30 20:56:03 -0500

    updated

--- app/models.py
+++ app/models.py
@@ -1,16 +1,3 @@
-'''
-from pydantic import BaseModel
-
-class IncidentData(BaseModel):
-    incident_time: str
-    grid_node: int
-
-class NewData(BaseModel):
-    incident_time: str
-    grid_node: int
-'''
-
-# test
 from pydantic import BaseModel, Field
 from bson import ObjectId
 from typing import Optional
@@ -18,7 +5,7 @@ from typing import Optional
 class User(BaseModel):
     id: str = Field(default_factory=str)
     username: str
-    hashed_password: Optional[str]  # Allow None for password
+    hashed_password: Optional[str]
     role: str
 
     class Config:
